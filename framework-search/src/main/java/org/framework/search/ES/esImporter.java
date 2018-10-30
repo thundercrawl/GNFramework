20c244905147376c4de60bd6f39d7f5984f49623
unknown <JINJUN SU@PROD.HCLPNP.COM>
2018-10-30 17:15:54 +0800

    udpate es search

---
+++ framework-search/src/main/java/org/framework/search/ES/esImporter.java
@@ -0,0 +1,25 @@
+package org.framework.search.ES;
+
+
+
+import java.io.IOException;
+
+import org.apache.http.HttpHost;
+import org.elasticsearch.action.ActionListener;
+import org.elasticsearch.action.admin.indices.alias.Alias;
+import org.elasticsearch.action.admin.indices.create.CreateIndexRequest;
+import org.elasticsearch.action.admin.indices.create.CreateIndexResponse;
+import org.elasticsearch.client.RequestOptions;
+import org.elasticsearch.client.RestClient;
+import org.elasticsearch.client.RestHighLevelClient;
+import org.elasticsearch.common.settings.Settings;
+import org.elasticsearch.common.unit.TimeValue;
+import org.elasticsearch.common.xcontent.XContentType;
+
+public class esImporter {
+
+	public static void main(String[] args) throws IOException {
+	
+	}
+
+}
